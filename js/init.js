c96016e0d61073d73b36035ec85c53fa60d5fe07
Katrina Wheelan <43075476+kwheelan@users.noreply.github.com>
2024-07-08 11:20:43 -0400

    changed ROOT to github root for data rendering

--- js/init.js
+++ js/init.js
@@ -3,9 +3,9 @@ import { loadPageState } from './utils/data_utils/local_storage_handlers.js'
 import { visitPage } from './views/view_logic.js'
 
 // path for my laptop
-export let DATA_ROOT = '../../../data/law_dept_sample/'
+// export let DATA_ROOT = '../../../data/law_dept_sample/'
 // github path
-// export let DATA_ROOT = '../../budget-request-demo/data/law_dept_sample/'
+export let DATA_ROOT = '../../budget-request-demo/data/law_dept_sample/'
 
 export let REVENUE = 0;
 export let TARGET = 2000000;
