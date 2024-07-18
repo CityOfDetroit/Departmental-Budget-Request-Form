e06776cdb69aee8701f1ce2862c9777316ca8c2e
Katrina Wheelan <katrina.wheelan@detroitmi.gov>
2024-07-18 15:45:49 -0400

    #31 added option to disable prompt buttons; move prompt above accordion; disable download and display message if above target

--- src/js/views/08_summary/helpers.js
+++ src/js/views/08_summary/helpers.js
@@ -5,7 +5,9 @@ import Subtitle from "../../components/header/header.js";
 import { visitPage } from "../view_logic.js";
 import { Accordion } from "../../components/accordion/accordion.js";
 import { downloadXLSX } from "../../utils/data_utils/XLSX_handlers.js";
-import Table from '../../components/table/table.js';
+import { Baseline } from '../../utils/data_utils/local_storage_handlers.js';
+import { TARGET } from '../../init.js';
+import { formatCurrency } from '../../utils/common_utils.js';
 
 export function summaryView(){
     Body.reset();
@@ -13,22 +15,36 @@ export function summaryView(){
     Accordion.show();
 
     // prompt buttons
-    Prompt.Text.update('');
-    Prompt.show();
-    Prompt.Buttons.Left.updateText('Download Excel');
-    Prompt.Buttons.Right.updateText('Go back to home');
+    Prompt.Buttons.Right.updateText('Download Excel');
+    Prompt.Buttons.Left.updateText('Go back to home');
+    // add button links
+    Prompt.Buttons.Left.addAction(returnToWelcome);
+    Prompt.Buttons.Right.addAction(downloadXLSX);
 
     // update page text
     Subtitle.update('Summary');
-    // add button links
-    Prompt.Buttons.Right.addAction(returnToWelcome);
-    Prompt.Buttons.Left.addAction(downloadXLSX);
-    
+
+    compareToTarget()
+}
+
+function compareToTarget(){
+    const baseline = new Baseline;
+    if (baseline.total() <= TARGET){
+        Prompt.Text.update(`Congrats! Your budget is below your target! 
+            Edit any line items below or download your completed Excel.`);
+        Prompt.show();
+    } else {
+        Prompt.Text.update(`Your budget is above your target of ${formatCurrency(TARGET)}. 
+            Please expand the summary table below and edit line items until you meet your target. 
+            When you meet the target, you will be able to download the Excel sheet.`);
+        Prompt.Buttons.Right.disable();
+        Prompt.show();
+    }
 }
 
 const returnToWelcome = () => {visitPage('welcome')}
 
 export function disablePromptButtons(){
-    Prompt.Buttons.Right.removeAction(returnToWelcome);
-    Prompt.Buttons.Left.removeAction(downloadXLSX);
+    Prompt.Buttons.Left.removeAction(returnToWelcome);
+    Prompt.Buttons.Right.removeAction(downloadXLSX);
 }
