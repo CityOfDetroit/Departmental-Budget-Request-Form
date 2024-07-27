1eef46c5584f2f818e5e21a5d3bbe77a7c9f232c
Katrina Wheelan <katrina.wheelan@detroitmi.gov>
2024-07-26 22:28:41 -0400

    adjust file upload view

--- src/js/views/01_upload/main.js
+++ src/js/views/01_upload/main.js
@@ -1,9 +1,29 @@
-import { CurrentPage } from "../../utils/data_utils/local_storage_handlers.js";
-import { initializePageView } from "./helpers.js";
+import { View } from '../view_class.js'
+
+import FileUpload from "../../components/file_upload/file_upload.js";
+import NavButtons from "../../components/nav_buttons/nav_buttons.js";
 
 export function loadUploadPage(){
-    //update page state
-    CurrentPage.update('upload');
-    initializePageView();
-    
+    var page = new UploadView();
+    page.visit();
+}
+
+class UploadView extends View {
+
+    constructor() {
+        super();
+        this.page_state = 'upload';
+        this.prompt = `Upload the baseline detail sheet given by your budget analyst.`;
+        this.subtitle = 'Excel Upload';
+        this.sidebar = false;
+    }
+
+    visit() {
+        super.visit();
+        // disable continue button until Excel file is uploaded and read
+        NavButtons.Next.disable();
+        FileUpload.show();
+        FileUpload.init();
+        NavButtons.Next.enable();
+    }
 }
