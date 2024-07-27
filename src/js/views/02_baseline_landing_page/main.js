92d5be9f319cfd3e476b0d559469156f1d71e34d
Katrina Wheelan <katrina.wheelan@detroitmi.gov>
2024-07-26 22:56:30 -0400

    update baseline to match new view class

--- src/js/views/02_baseline_landing_page/main.js
+++ src/js/views/02_baseline_landing_page/main.js
@@ -1,10 +1,96 @@
-import { CurrentPage } from "../../utils/data_utils/local_storage_handlers.js";
-import { preparePageView, initializeFundTable } from "../02_baseline_landing_page/helpers.js";
-
+import NavButtons from "../../components/nav_buttons/nav_buttons.js";
+import Table from "../../components/table/table.js";
+import { View, ViewTable } from '../view_class.js'
+import { CurrentFund } from "../../utils/data_utils/local_storage_handlers.js";
 
 export function loadBaselineLandingPage(){
-    //update page state
-    CurrentPage.update('baseline-landing');
-    preparePageView();
-    initializeFundTable();
+
+    var page = new FundView();
+    page.visit();
+
+}
+
+class FundView extends View {
+
+    constructor() {
+        super();
+        this.page_state = 'baseline-landing';
+        this.prompt = `We will now ask you a series of questions about your BASELINE budget request.
+            At the end, we will ask you about any new initiatives (ie. supplemental requests).
+            Select one of your funds then click continue.`;
+        this.subtitle = 'Baseline Budget Request';
+        this.table = new FundTable();
+        this.sidebar = false;
+    }
+
+    visit() {
+        // remove fund selection
+        localStorage.setItem("fund", '');
+        super.visit();
+    }
+}
+
+class FundTable extends ViewTable {
+
+    constructor() {
+        super();
+
+        // add additional revenue columns to the table
+        this.columns = [
+            { title: 'Fund', className: 'fund-name' }
+        ];
+
+        this.noDataMessage = 'No funds found.'
+        this.addEdit = false;
+    }
+
+    async build(){
+        await super.build();
+
+        // Table.Data.loadFunds();
+        // Table.show();
+        NavButtons.Next.disable();
+        // Table.Columns.assignClasses(this.columns);
+
+        Table.adjustWidth('30%');
+        allowRowSelection();
+    }
+}
+
+
+function allowRowSelection(){
+    
+    var tableRows = document.querySelectorAll("tbody tr");
+    
+    // enable highlight on hover and on select
+    tableRows.forEach(function(row) {
+        row.addEventListener('mouseover', function() {
+            this.classList.add('hover-effect');
+        });
+        row.addEventListener('mouseout', function() {
+            this.classList.remove('hover-effect');
+        });
+        row.addEventListener('click', function() {
+            selectFund(tableRows, this);
+        });
+    });
+}
+
+function selectFund(tableRows, selected_row){
+
+    // remove selected class from any other rows
+    tableRows.forEach(function(tableRow) {
+        tableRow.classList = '';
+    });
+
+    // add selected class to clicked row
+    selected_row.classList.add('selected');
+
+    // get fund and save selected fund
+    var fund = selected_row.querySelector('.fund-name').textContent;
+    var fundNumber = parseInt(fund);
+    CurrentFund.update(fundNumber);
+
+    // enable next step
+    NavButtons.Next.enable();
 }
