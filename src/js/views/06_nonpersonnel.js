509331283240f7ee4224e1c0e402c50842e70f5b
Katrina Wheelan <katrina.wheelan@detroitmi.gov>
2024-08-14 13:24:52 -0400

    nonpersonnel edits

--- src/js/views/06_nonpersonnel.js
+++ src/js/views/06_nonpersonnel.js
@@ -26,19 +26,19 @@ class NonPersonnelTable extends ViewTable {
             { title: `FY${fiscal_year} Departmental Request Total`, className: 'request', isCost: true },
             { title: 'Service', className : 'service' },
             { title: 'Recurring or One-Time', className: 'recurring'},
+            { title: 'Vendor Name', className: 'vendor'},
             { title : 'CPA #', className : 'cpa'},
             // hidden columns
             { title: 'End Date', className: 'contract-end', hide: true},
             { title: 'BPA/CPA Amount Remaining', className: 'remaining', isCost: true , hide: true},
             { title: 'Object Name', className: 'object-name', hide: true},
             { title: 'Object', className: 'object', hide: true},
-            { title: 'Vendor Name', className: 'vendor', hide: true},
             { title: 'Object Category', className: 'object-category', hide: true},
             { title: 'BPA/CPA Description', className: 'cpa-description', hide: true} 
         ]);
 
         this.noDataMessage = 'No non-personnel expenditures for this fund.'
-        this.addButtonText = 'Add new job' ;
+        this.addButtonText = 'Add new non-personnel item' ;
     }
 
     // action on row edit click: make cells editable
